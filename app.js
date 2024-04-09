bfc445110ca570fd16990e4d679a43c9bcec69b0
bharathraj13 <13bharathraj@gmail.com>
2024-04-09 09:58:47 +0530

    9-4 design change

--- app.js
+++ app.js
@@ -165,7 +165,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log('Serving on port ' + port);
